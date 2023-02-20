2c8cebace7e6b43e940831839e7404b3fa75f508
duy366110 <duy366110@gmail.com>
2023-02-20 17:45:23 +0700

    #Commit 20/02/2023: Done lap - 01.

---
+++ lession_01/js/main.js
@@ -0,0 +1,7 @@
+let country = 8;
+let continent = 15;
+let population = 10;
+
+let template = `country ${8} triệu dân - continent ${15} triệu dân - population ${10} tỷ người`;
+
+console.log(template);
