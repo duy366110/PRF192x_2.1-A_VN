5b599af4bfd8f6b091c3ea1bcf0a5ac1206dafc3
duy366110 <duy366110@gmail.com>
2023-03-02 17:03:33 +0700

    #Commit 02/03/2023: Config validation template controller v2.

---
+++ Assignment_01/js/script/data.js
@@ -0,0 +1,11 @@
+export const messageErrors = {
+    age: 'Age must be between 1 and 15!',
+    breed: 'Please select Breed!',
+    length: 'Length must be between 1 and 100!',
+    number: 'Please enter number',
+    required: 'Please enter content',
+    uniqueID: 'ID must be unique!',
+    type: 'Please select Type!',
+    weight: 'Weight must be between 1 and 15!',
+
+}
